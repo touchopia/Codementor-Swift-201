13f94d0b7b1566a9b9f2420fb0af2157db335a6d
Phil Wright <touchopia@me.com>
2015-09-04 08:23:03 -0600

    Adding SourceCode

---
+++ SourceCode/StickersApp/StickersApp/ViewController.swift
@@ -0,0 +1,45 @@
+//
+//  ViewController.swift
+//  StickersApp
+//
+//  Created by Phil Wright on 9/1/15.
+//  Copyright © 2015 Touchopia, LLC. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        let backGroundImageView = UIImageView(image: UIImage(named:"blueBackground"))
+        view.addSubview(backGroundImageView)
+        
+        let imageView = PinchZoomImageView(image: UIImage(named: "1"))
+        let imageView1 = PinchZoomImageView(image: UIImage(named: "2"))
+        let imageView2 = PinchZoomImageView(image: UIImage(named: "3"))
+        
+    
+        imageView.frame.origin = randomOriginPoint()
+        imageView1.frame.origin = randomOriginPoint()
+        imageView2.frame.origin = randomOriginPoint()
+        
+        view.addSubview(imageView)
+        view.addSubview(imageView1)
+        view.addSubview(imageView2)
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    func randomOriginPoint() -> CGPoint {
+        let screenWidth = UInt32(self.view.frame.size.width / 2)
+        
+        return CGPointMake(CGFloat(arc4random_uniform(screenWidth)), CGFloat(arc4random_uniform(screenWidth)))
+    }
+
+}
+
