ca3815f0ea77533209be2b36c3e9f3b0d7525342
Phil Wright <touchopia@me.com>
2015-09-14 17:23:18 -0600

    Adding simple TapGestures Project

---
+++ SourceCode/TapGestures/TapGestures/ViewController.swift
@@ -0,0 +1,32 @@
+//
+//  ViewController.swift
+//  TapGestures
+//
+//  Created by Phil Wright on 9/14/15.
+//  Copyright © 2015 Touchopia, LLC. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController, UIGestureRecognizerDelegate {
+
+    @IBOutlet weak var characterImageView: UIImageView!
+
+    var characterOneTapGesture: UITapGestureRecognizer!
+
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        let characterOneTapGesture = UITapGestureRecognizer(target: self, action:Selector("handleTap:"))
+        characterOneTapGesture.delegate = self
+        characterImageView.addGestureRecognizer(characterOneTapGesture)
+    
+    }
+
+    func handleTap(sender: AnyObject) {
+        debugPrint("tapped on one")
+    }
+    
+}
+
