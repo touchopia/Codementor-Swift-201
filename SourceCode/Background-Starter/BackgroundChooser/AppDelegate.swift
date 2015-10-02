e69a0b6bd3e933bf43b82293875c5a65c86d173c
Phil Wright <touchopia@me.com>
2015-10-01 18:15:07 -0600

    adding background starter project

---
+++ SourceCode/Background-Starter/BackgroundChooser/AppDelegate.swift
@@ -0,0 +1,23 @@
+//
+//  AppDelegate.swift
+//  BackgroundChooser
+//
+//  Created by Phil Wright on 10/1/15.
+//  Copyright © 2015 Touchopia LLC. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+
+
+    func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
+        // Override point for customization after application launch.
+        return true
+    }
+
+}
+
